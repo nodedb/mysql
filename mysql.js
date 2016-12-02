93bad511dd1184cb94d5146834aa9a92dc82ded4
Simon Emms <simon@simonemms.com>
2016-12-02 07:17:40 +0000

    More DB stuff

--- mysql.js
+++ mysql.js
@@ -6,22 +6,75 @@
 
 
 /* Node modules */
+const fs = require("fs");
 
 
 /* Third-party modules */
-const angular = require("angular");
 
 
 /* Files */
 
 
-export const module = "vanilladb-mysql";
-export const app = angular.module(module, []);
+module.exports = angular => {
 
+    const module = "nodedb-mysql";
+    const app = angular.module(module, []);
 
-app.factory("vdbMysql", () => {
+    app
+        .controller(`${module}.DatabaseListCtrl`, function (currentConnection) {
 
-    return "hello";
+            this.eg = "some input";
 
-});
+        })
+        .factory(`${module}.databaseListTpl`, () => fs.readFileSync(`${__dirname}/views/dbList.html`))
+        .controller(`${module}.MenubarCtrl`, function (currentConnection) {
 
+            this.eg = "some input";
+
+        })
+        .factory(`${module}.menuBarTpl`, () => fs.readFileSync(`${__dirname}/views/menubar.html`))
+        .controller(`${module}.ViewportCtrl`, function (currentConnection) {
+
+            this.eg = "some input";
+
+        })
+        .factory(`${module}.ViewportTpl`, () => fs.readFileSync(`${__dirname}/views/viewport.html`))
+        .factory(`${module}.strategy`, require("./lib/strategy"))
+        .factory(`${module}.connectForm`, () => {
+
+            return {
+                type: "object",
+                properties: {
+                    host: {
+                        type: "string",
+                        default: "localhost"
+                    },
+                    user: {
+                        type: "string"
+                    },
+                    password: {
+                        type: "string",
+                        "x-schema-form": {
+                            type: "password"
+                        }
+                    },
+                    database: {
+                        type: "string"
+                    },
+                    port: {
+                        type: "integer",
+                        default: 3306,
+                        minimum: 0,
+                        exclusiveMinimum: true
+                    }
+                }
+            };
+
+        });
+
+    return {
+        app,
+        module
+    };
+
+};
